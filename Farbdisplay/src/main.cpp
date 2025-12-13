e83dcb777a2f650d4636fb2ca605a7decebd01b6
Luca677 <SchregerLuca@student.gsso.de>
2025-12-13 20:14:57 +0100

    Libs

--- Farbdisplay/src/main.cpp
+++ Farbdisplay/src/main.cpp
@@ -1,91 +1,35 @@
-#include <Arduino.h>
 #include <SPI.h>
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 
-#define cs PB0 // Chip Select An/Aus, Low = Display Aktiv 
-#define rst PB1 // Reset Display, Low = Reset
-#define dc PB2 // Data/Command, Low = Command, High = Data
-#define led PB12 // Backlight An/Aus, High = An
+// ===== PIN-DEFINITION =====
+#define TFT_CS   PB6
+#define TFT_DC   PB7
+#define TFT_RST  PB8
 
-#define sck PB3 // Serial Clock
-#define miso PB4 // Master In Slave Out
-#define mosi PB5 // Master Out Slave In
-
-void writeCommand(uint8_t cmd) {
-  digitalWrite(dc, LOW);
-  digitalWrite(cs, LOW);
-  SPI.transfer(cmd);
-  digitalWrite(cs, HIGH);
-}
-
-void writeData(uint8_t data) {
-  digitalWrite(dc, HIGH);
-  digitalWrite(cs, LOW);
-  SPI.transfer(data);
-  digitalWrite(cs, HIGH);
-}
-
-void tftInit() {
-  // Reset
-  digitalWrite(rst, LOW);
-  delay(50);
-  digitalWrite(rst, HIGH);
-  delay(120);
-
-  // Sleep Out
-  writeCommand(0x11);
-  delay(120);
-
-  // Display ON
-  writeCommand(0x29);
-}
-
-void fillScreen(uint16_t color) {
-  // Column Address Set
-  writeCommand(0x2A);
-  writeData(0 >> 8);
-  writeData(0 & 0xFF);
-  writeData((240 - 1) >> 8);
-  writeData((240 - 1) & 0xFF);
-
-  // Page Address Set
-  writeCommand(0x2B);
-  writeData(0 >> 8);
-  writeData(0 & 0xFF);
-  writeData((320 - 1) >> 8);
-  writeData((320 - 1) & 0xFF);
-
-  // RAM write
-  writeCommand(0x2C); //Display erwartet Pixeldaten
-
-  digitalWrite(dc, HIGH);
-  digitalWrite(cs, LOW);
-  for (int i = 0; i < 240 * 320; i++) {
-    SPI.transfer(color >> 8);
-    SPI.transfer(color & 0xFF);
-  }
-  digitalWrite(cs, HIGH);
-}
-
+// ===== DISPLAY =====
+Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  pinMode(cs, OUTPUT);
-  pinMode(rst, OUTPUT);
-  pinMode(dc, OUTPUT);
-  pinMode(led, OUTPUT);
+  // SPI-PINS FEST setzen (EXTREM WICHTIG)
+  SPI.setMOSI(PA7);
+  SPI.setMISO(PA6);
+  SPI.setSCK(PA5);
 
-  digitalWrite(led, HIGH);
+  SPI.begin();
 
-  // SPI starten
-  SPI.begin();  // nutzt automatisch PA5/PA6/PA7
+  tft.begin();
 
-  tftInit(); // Display starten
+  // Rotation korrekt setzen (verhindert schwarze Bereiche!)
+  tft.setRotation(1); // 0..3 ausprobieren falls nötig
 
-  fillScreen(0xF800);  // ROT anzeigen mit Wert 11111(R) 000000(G) 00000(B)
+  // Bildschirm löschen
+  tft.fillScreen(ILI9341_BLACK);
 
+  // TEST: kompletter Screen ROT
+  tft.fillScreen(ILI9341_RED);
 }
 
 void loop() {
-  
 }
-
-
