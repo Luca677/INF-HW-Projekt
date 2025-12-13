e4e26ae394718d916782568c8a16554e8f85ece5
Luca677 <SchregerLuca@student.gsso.de>
2025-12-13 17:35:27 +0100

    änderungen

--- Farbdisplay/src/main.cpp
+++ Farbdisplay/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <SPI.h>
+
 #define cs PB0 // Chip Select An/Aus, Low = Display Aktiv 
 #define rst PB1 // Reset Display, Low = Reset
 #define dc PB2 // Data/Command, Low = Command, High = Data
@@ -8,26 +10,77 @@
 #define miso PB4 // Master In Slave Out
 #define mosi PB5 // Master Out Slave In
 
+void writeCommand(uint8_t cmd) {
+  digitalWrite(dc, LOW);
+  digitalWrite(cs, LOW);
+  SPI.transfer(cmd);
+  digitalWrite(cs, HIGH);
+}
+
+void writeData(uint8_t data) {
+  digitalWrite(dc, HIGH);
+  digitalWrite(cs, LOW);
+  SPI.transfer(data);
+  digitalWrite(cs, HIGH);
+}
+
+void tftInit() {
+  // Reset
+  digitalWrite(rst, LOW);
+  delay(50);
+  digitalWrite(rst, HIGH);
+  delay(120);
+
+  // Sleep Out
+  writeCommand(0x11);
+  delay(120);
+
+  // Display ON
+  writeCommand(0x29);
+}
+
+void fillScreen(uint16_t color) {
+  // Column Address Set
+  writeCommand(0x2A);
+  writeData(0 >> 8);
+  writeData(0 & 0xFF);
+  writeData((240 - 1) >> 8);
+  writeData((240 - 1) & 0xFF);
+
+  // Page Address Set
+  writeCommand(0x2B);
+  writeData(0 >> 8);
+  writeData(0 & 0xFF);
+  writeData((320 - 1) >> 8);
+  writeData((320 - 1) & 0xFF);
+
+  // RAM write
+  writeCommand(0x2C);
+
+  digitalWrite(dc, HIGH);
+  digitalWrite(cs, LOW);
+  for (int i = 0; i < 240 * 320; i++) {
+    SPI.transfer(color >> 8);
+    SPI.transfer(color & 0xFF);
+  }
+  digitalWrite(cs, HIGH);
+}
+
+
 void setup() {
-  pinMode(cs, OUTPUT); // Chip Select als Ausgang
-  digitalWrite(cs, LOW); // Display aktivieren
+  pinMode(cs, OUTPUT);
+  pinMode(rst, OUTPUT);
+  pinMode(dc, OUTPUT);
+  pinMode(led, OUTPUT);
 
-  pinMode(rst, OUTPUT); // Reset als Ausgang
-  digitalWrite(rst, LOW); // Display zurücksetzen
-  delay(100); // Kurze Pause
-  digitalWrite(rst, HIGH); // Display aus dem Reset holen
+  digitalWrite(led, HIGH);
 
-  pinMode(dc, OUTPUT); // Data/Command als Ausgang
-  digitalWrite(dc, HIGH); // Auf Datenmodus setzen
+  // SPI starten
+  SPI.begin();  // nutzt automatisch PA5/PA6/PA7
 
-  pinMode(led, OUTPUT); // Backlight als Ausgang
-  digitalWrite(led, HIGH); // Backlight einschalten
+  tftInit(); // Display starten
 
-  pinMode(sck, OUTPUT); // Serial Clock als Ausgang
-
-  pinMode(miso, INPUT); // Master In Slave Out als Eingang
-  
-  pinMode(mosi, OUTPUT); // Master Out Slave In als Ausgang
+  fillScreen(0xF800);  // ROT anzeigen
 
 }
 
