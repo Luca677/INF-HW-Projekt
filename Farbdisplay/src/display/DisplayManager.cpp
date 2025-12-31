0eeca57871ec89d18ab293cab01df4a7cd3f54e9
Luca677 <SchregerLuca@student.gsso.de>
2025-12-31 17:29:52 +0100

    Buttons gehen jetzt

--- Farbdisplay/src/display/DisplayManager.cpp
+++ Farbdisplay/src/display/DisplayManager.cpp
@@ -5,6 +5,7 @@ DisplayManager::DisplayManager(uint8_t cs, uint8_t dc, uint8_t rst)
 
 void DisplayManager::begin() {
     _tft.begin();
+    _tft.setSPISpeed(40000000); // 40 MHz sonst 20MHz
     _tft.setRotation(1);
 }
 
--- Farbdisplay/src/ui/HomePage.cpp
+++ Farbdisplay/src/ui/HomePage.cpp
@@ -1,7 +1,7 @@
 #include "HomePage.h"
 #include "assets/menu_bitmap.h"
-#define BTN_W 180
-#define BTN_H 50
+#define BTN_W 158
+#define BTN_H 64
 
 static bool inRect(int x, int y, int w, int h, int tx, int ty) {
     return tx >= x && tx < x + w && ty >= y && ty < y + h;
@@ -12,15 +12,17 @@ void HomePage::draw(Adafruit_ILI9341& tft) {
 }
 
 PageID HomePage::handleTouch(int x, int y) {
+    //
+    if (inRect(0, 62, BTN_W, BTN_H, x, y)) {
+        return PageID::PAINT;
+    }
     // Settings Button (70,50,180,50) -> y=50..100
-    if (inRect(70, 50, BTN_W, BTN_H, x, y))
+    if (inRect(160, 100, BTN_W, BTN_H, x, y))
         return PageID::SETTINGS;
 
     // Info Button (70,120,180,50) -> y=120..170
-    if (inRect(70, 120, BTN_W, BTN_H, x, y))
+    if (inRect(0, 140, BTN_W, BTN_H, x, y))
         return PageID::INFO;
-    if (inRect(80, 95, 160, 50, x, y)) {
-        return PageID::PAINT;
-    }
+    
     return PageID::HOME;
 }
